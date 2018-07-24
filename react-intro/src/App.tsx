99125595f122e1a62ffd178fc7edb9908743d9c7
Petersen <apetersen@skylinetechnologies.com>
2018-07-24 15:54:18 -0500

    got parcel hooked up

---
+++ react-intro/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export default class App extends React.Component<AppProps, {}> {
+    render() {
+        return (
+            <div>Hi There</div>
+        );
+    }
+}
+
+export interface AppProps {
+    //props
+}
