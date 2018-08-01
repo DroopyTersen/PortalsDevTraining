607f1dc878332caad8eff928bfc493a37448ea93
Petersen <apetersen@skylinetechnologies.com>
2018-07-31 23:11:16 -0500

    removed createAppCatalogs script

--- createAppCatalogs.ps1
+++
@@ -1,6 +0,0 @@
-o365 spo connect https://skylinespark-admin.sharepoint.com -t password -u apetersen@skylinespark.onmicrosoft.com -p SHHH 
-for ($i = 1; $i -le 8; $i++) {
-    $url = "https://skylinespark.sharepoint.com/sites/Oneida-" + $i
-    Write-Host "Creating Site Collection App Catalog for: " $url
-    o365 spo site appcatalog add --url $url
-}
